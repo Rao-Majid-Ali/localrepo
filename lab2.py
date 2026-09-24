ade8d6b9ff9c12ce46170281dee216580f13ad8f
Majid-Ali-Coder <majidalirao786786@email.com>
2026-09-24 10:53:39 +0500

    Add lab2 python file

--- lab2.py
+++ lab2.py
@@ -3,3 +3,10 @@ count = 0
 while (count < 3): 
     count = count + 1 
     print("Hello Geek")
+
+# Python program to illustrate 
+# Iterating over a list 
+print("List Iteration") 
+l = ["geeks", "for", "geeks"] 
+for i in l: 
+print(i) 
