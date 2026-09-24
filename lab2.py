a88a71b750587d6d0020dd13e2b793231ee866c2
Majid-Ali-Coder <majidalirao786786@email.com>
2026-09-24 10:57:52 +0500

    Add lab2 python file

--- lab2.py
+++ lab2.py
@@ -10,3 +10,10 @@ print("List Iteration")
 l = ["geeks", "for", "geeks"] 
 for i in l: 
 print(i) 
+
+
+ Iterating over a tuple (immutable) 
+print("\nTuple Iteration") 
+t = ("geeks", "for", "geeks") 
+for i in t: 
+print(i) 
